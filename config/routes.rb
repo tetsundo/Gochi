cc73facd12583917bc414eea434613ab3d94f6af
Masa08 <ushimasarug08@gmail.com>
2018-05-04 09:24:01 +0000

    devise routing change

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :stores
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  devise_for :stores, controllers: {
+    sessions: 'stores/sessions',
+    passwords: 'stores/passwords',
+    registrations: 'stores/registrations'
+  }
 
   resources :users
   resources :stores
