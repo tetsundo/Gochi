f5bf3edd0c911c077c2a5f3e0bacc357df9acc69
Masa08 <ushimasarug08@gmail.com>
2018-05-04 08:27:57 +0000

    routing changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,8 @@
 Rails.application.routes.draw do
-  
-  devise_for :stores
   devise_for :users
-  get 'users/edit'
+  devise_for :stores
 
-  get 'users/show'
-
-  get 'staffs/new'
-
-  get 'staffs/create'
-
-  get 'staffs/index'
-
-  get 'staffs/edit'
-
-  get 'thanks/new'
-
-  get 'thanks/create'
-
-  get 'thanks/complete'
-
-  get 'stores/edit'
-
-  get 'stores/show'
-
-  get 'stores/index'
-
-  get 'gochies/index'
-
-  get 'gochies/new'
-
-  get 'gochies/complete'
-
-  get 'gochies/show'
-
+  resources :users
+  resources :stores
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
