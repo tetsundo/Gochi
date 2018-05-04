547ef34523c2ce65a0e0f1ebef3a7268a2731a54
Ndo <tetsundo0156justdoit@gmail.com>
2018-05-04 11:20:16 +0000

    Store controller update  view show edit

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,6 +2,11 @@ class StoresController < ApplicationController
   def edit
   	@store = Store.find(params[:id])
   end
+  def update
+  	@store = Store.find(params[:id])
+  	@store.update(store_params)
+    redirect_to store_path(@store.id)
+  end
 
   def show
   	@store = Store.find(params[:id])
@@ -10,4 +15,10 @@ class StoresController < ApplicationController
   def index
   	@stores = Store.all
   end
+
+  private
+
+	def store_params
+	    params.require(:store).permit(:name, :image, :intro, :email)
+	end
 end
