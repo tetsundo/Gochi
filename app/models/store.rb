f3e9dae8b1b7d25ea6db89577d185e671b4364b6
Ndo <tetsundo0156justdoit@gmail.com>
2018-05-05 02:17:07 +0000

    merged staffs into store

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,8 @@ class Store < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable
+         :recoverable, :rememberable, :trackable,:validatable
 
    attachment :image
+  has_many :staffs
 end
